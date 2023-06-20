7bc245f2b4351a89cd8bdf6bf8c42a73e244be27
nastiaknik <anastazja.knihnitska@gmail.com>
2023-06-21 00:21:59 +0200

    initialize react app and add favicon

---
+++ src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: 40,
+        color: "#010101",
+      }}
+    >
+      Goose Track
+    </div>
+  );
+}
+
+export default App;
