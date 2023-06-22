7a0f6c69b90caa8c0ce89f1f27722ea778ac4378
Иван Чмиль <chmil.ivan.u@gmail.com>
2023-06-22 23:53:54 +0300

    +icon

--- src/components/MainLayout/ThemeToggler/ThemeToggler.js
+++ src/components/MainLayout/ThemeToggler/ThemeToggler.js
@@ -1,11 +1,13 @@
 import { useDispatch, useSelector } from "react-redux";
+import { FiMoon } from "react-icons/fi";
+import { FiSun } from "react-icons/fi";
 import { toggleTheme } from "redux/theme/themeSlice"
 export const ThemeToggler = () => {
     const currentTheme = useSelector((state) => state.theme.currentTheme);
     const dispatch = useDispatch()
     return (
       <button onClick={() => dispatch(toggleTheme())}>
-        {currentTheme ? <p> light</p> : <p> dark</p>}
+        {currentTheme ? <p> <FiMoon/> </p> : <p> <FiSun/> </p>}
       </button>
     );
 }
