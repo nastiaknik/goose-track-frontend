30cc16c7f979772774259d451cceba1f7ad9eddd
nastiaknik <anastazja.knihnitska@gmail.com>
2023-06-23 00:24:57 +0200

    resolves conflicts with variables

--- src/styles/themes.jsx
+++ src/styles/themes.jsx
@@ -1,36 +1,53 @@
+import { useSelector } from "react-redux";
 import { ThemeProvider } from "styled-components";
-import { GlobalStyles } from "./GlobalStyles";
-import { StyleVariables } from "./StyleVariables";
+import { GlobalStyle } from "./globalStyle";
 
 // *** ТІЛЬКИ ДЛЯ ТИХ ЄЛЕМЕНТІВ ЩО ЗМІНЮЮТЬ КОЛІР ***
 // ПРИКЛАД ЯК ОБРАТИ ПОТРІБНИЙ КОЛІР У КОМПОНЕНТІ
-// color:${({theme})=>theme.colors.backgroun2}
+// color:${({theme})=>theme.variableColors.primaryBackground}
 
 const lightTheme = {
-  colors: {
+  variableColors: {
     sideBar: "#FFFFFF", // колір бокової панелі
-    backgroun: "#F7F6F9", // колір сомого нижнього фону
-    backgroun2: "#FFFFFF", // колір фону
+    primaryBackground: "#F7F6F9", // колір сомого нижнього фону
+    secondaryBackground: "#FFFFFF", // колір фону
     buttonBlue: "#E3F3FF", // колір кнопок
     text: "#111111", // колір тексту
   },
 };
 
-// const darkTheme = {
-//   colors: {
-//     sideBar: "#13151A", // колір бокової панелі
-//     backgroun: "#171820", // колір сомого нижнього фону
-//     backgroun2: "#21222C", // колір фону
-//     buttonBlue: "#3E85F3", // колір кнопок
-//     text: "#FFFFFF", // колір тексту
-//   },
-// };
+const darkTheme = {
+  variableColors: {
+    sideBar: "#13151A", // колір бокової панелі
+    primaryBackground: "#171820", // колір сомого нижнього фону
+    secondaryBackground: "#21222C", // колір фону
+    buttonBlue: "#3E85F3", // колір кнопок
+    text: "#FFFFFF", // колір тексту
+  },
+};
+
+// *** ВСІ ІНШІ КОЛЬОРИ *** ДОДАВАТИ СЮДИ
+// ПРИКЛАД ЯК ОБРАТИ ПОТРІБНИЙ КОЛІР У КОМПОНЕНТІ
+// color:${({theme})=>theme.testcolor1}
+
+const otherColors = {
+  testcolor1: "#ff0000",
+  testcolor2: "#ff00ff",
+};
 
 export const Theme = ({ children }) => {
+  const currentTheme = useSelector((state) => state.theme.currentTheme);
+  const theme = (checkTheme = true) => {
+    return checkTheme
+      ? { ...lightTheme, ...otherColors }
+      : { ...darkTheme, ...otherColors };
+  };
+  console.log(currentTheme);
+  console.log(theme(currentTheme));
+
   return (
-    <ThemeProvider theme={lightTheme}>
-      <GlobalStyles />
-      <StyleVariables />
+    <ThemeProvider theme={theme(currentTheme)}>
+      <GlobalStyle />
       {children}
     </ThemeProvider>
   );
