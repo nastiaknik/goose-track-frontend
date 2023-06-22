a7cbc4d20332b377db3b9b7460e80ec737f8c1a3
Иван Чмиль <chmil.ivan.u@gmail.com>
2023-06-22 12:36:20 +0300

    miniUpdate

--- src/styles/themes.jsx
+++ src/styles/themes.jsx
@@ -3,10 +3,10 @@ import { GlobalStyle } from "./globalStyle";
 
 // *** ТІЛЬКИ ДЛЯ ТИХ ЄЛЕМЕНТІВ ЩО ЗМІНЮЮТЬ КОЛІР ***
 // ПРИКЛАД ЯК ОБРАТИ ПОТРІБНИЙ КОЛІР У КОМПОНЕНТІ
-// color:${({theme})=>theme.colors.backgroun2}
+// color:${({theme})=>theme.variableColors.background2}
 
 const lightTheme = {
-  colors: {
+  variableColors: {
     sideBar: "#FFFFFF", // колір бокової панелі
     backgroun: "#F7F6F9", // колір сомого нижнього фону
     backgroun2: "#FFFFFF", // колір фону
@@ -15,21 +15,40 @@ const lightTheme = {
   },
 };
 
-// const darkTheme = {
-//   colors: {
-//     sideBar: "#13151A", // колір бокової панелі
-//     backgroun: "#171820", // колір сомого нижнього фону
-//     backgroun2: "#21222C", // колір фону
-//     buttonBlue: "#3E85F3", // колір кнопок 
-//     text: "#FFFFFF", // колір тексту 
-//   },
-// };
+const darkTheme = {
+  variableColors: {
+    sideBar: "#13151A", // колір бокової панелі
+    background: "#171820", // колір сомого нижнього фону
+    background2: "#21222C", // колір фону
+    buttonBlue: "#3E85F3", // колір кнопок
+    text: "#FFFFFF", // колір тексту
+  },
+};
 
+// *** ВСІ ІНШІ КОЛЬОРИ *** ДОДАВАТИ СЮДИ
+// ПРИКЛАД ЯК ОБРАТИ ПОТРІБНИЙ КОЛІР У КОМПОНЕНТІ
+// color:${({theme})=>theme.testcolor}
+
+const otherColors = {
+  testcolor1: "#ff0000",
+  testcolor2:"#ff00ff"
+}
+ 
+// КОЛИ БУДЕ КНОПКА ЗМІНИ КОЛЬОРУ ТО ПРИДУМАЄМ ЯК ПЕРЕДАВАТИ СЮДИ ТРУ АБО ФОЛС
+// поки за потреби міняєму руцями)
+
+const theme = (chekboxTheme = true) => {
+  return chekboxTheme
+    ? { ...lightTheme, ...otherColors }
+    : { ...darkTheme, ...otherColors };
+  
+}
+//  console.log(theme())
 export const Theme = ({children}) => {
     return (
-        <ThemeProvider theme={lightTheme}>
-            <GlobalStyle/>
-            {children}
-        </ThemeProvider>
-    )
+      <ThemeProvider theme={theme()}>
+        <GlobalStyle />
+        {children}
+      </ThemeProvider>
+    );
 }
