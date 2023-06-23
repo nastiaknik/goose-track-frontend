1b555961106406cbe880bb03bc58b6185033a5b1
IrynaVal <piv8050@gmail.com>
2023-06-23 17:28:13 +0300

    update Modal

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,26 +1,5 @@
-// import { Modal } from "components/SharedComponents/Modal/Modal";
-// import { useState } from "react";
-
 function MainPage() {
-  // const [isModalOpen, setIsModalOpen] = useState(false);
-
-  // const toggleModal = () => {
-  //   if (isModalOpen) {
-  //     setIsModalOpen(false);
-  //   } else {
-  //     setIsModalOpen(true);
-  //   }
-  // };
-
-  return (
-    <div>
-      MainPage
-      {/* <button type="button" onClick={toggleModal}>
-        Open modal
-      </button>
-      {isModalOpen && <Modal onClose={toggleModal} />} */}
-    </div>
-  );
+  return <div>MainPage</div>;
 }
 
 export default MainPage;
