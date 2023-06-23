5570bdb130e5071cfafa0c9a455727a6f96436c9
IrynaVal <piv8050@gmail.com>
2023-06-23 21:23:41 +0300

    add AddTaskBtn+updated Modal

---
+++ src/components/CalendarPage/AddTaskBtn/AddTaskBtn.js
@@ -0,0 +1,10 @@
+// import PropTypes from "prop-types";
+
+export const AddTaskBtn = ({ id, onClick }) => {
+  return <button type="button">Add task</button>;
+};
+
+// AddTaskBtn.propTypes = {
+//   id: PropTypes.element.isRequired,
+//   onClick: PropTypes.func.isRequired,
+// };
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -2,12 +2,30 @@ import { AuthSection } from "components/MainPage/AuthSection/AuthSection";
 import { Description } from "components/MainPage/Description/Description";
 import { ReviewsSlider } from "components/MainPage/ReviewsSlider/ReviewsSlider";
 
+import { Modal } from "components/SharedComponents/Modal/Modal";
+import { useState } from "react";
+
 function MainPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    if (isModalOpen) {
+      setIsModalOpen(false);
+    } else {
+      setIsModalOpen(true);
+    }
+  };
+
   return (
     <>
       <AuthSection />
       <Description />
       <ReviewsSlider />
+
+      <button type="button" onClick={toggleModal}>
+        Open modal
+      </button>
+      {isModalOpen && <Modal onClose={toggleModal} />}
     </>
   );
 }
