3273299ac9398551ed36ca9cc2882b81b3ab6c9f
IrynaVal <piv8050@gmail.com>
2023-06-24 17:55:16 +0300

    updated AddTaskBtn

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -2,33 +2,12 @@ import { AuthSection } from "components/MainPage/AuthSection/AuthSection";
 import { Description } from "components/MainPage/Description/Description";
 import { ReviewsSlider } from "components/MainPage/ReviewsSlider/ReviewsSlider";
 
-import { Modal } from "components/SharedComponents/Modal/Modal";
-import { AddTaskBtn } from "components/CalendarPage/AddTaskBtn/AddTaskBtn";
-import { useState } from "react";
-
 function MainPage() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const toggleModal = () => {
-    if (isModalOpen) {
-      setIsModalOpen(false);
-    } else {
-      setIsModalOpen(true);
-    }
-  };
-
   return (
     <>
       <AuthSection />
       <Description />
       <ReviewsSlider />
-
-      {/* <button type="button" onClick={toggleModal}>
-        Open modal
-      </button> */}
-      <AddTaskBtn onClick={toggleModal} />
-
-      {isModalOpen && <Modal onClose={toggleModal} />}
     </>
   );
 }
