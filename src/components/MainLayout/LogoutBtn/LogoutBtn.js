d0badef828e65bb9ab408a0f6c8850bbec492499
Milosska <110258611+Milosska@users.noreply.github.com>
2023-06-24 21:11:25 +0300

    adds all auth redux operation, axios innstance, and loader

--- src/components/MainLayout/LogoutBtn/LogoutBtn.js
+++ src/components/MainLayout/LogoutBtn/LogoutBtn.js
@@ -1,10 +1,19 @@
+import { useDispatch } from "react-redux";
+import { logout } from "redux/auth/operations";
+
 import { Button } from "./LogoutBtn.styled";
 import { FiLogOut } from "react-icons/fi";
 
 export const LogOut = () => {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
     <>
-      <Button>
+      <Button type="button" onClick={handleLogout}>
         Log Out
         <FiLogOut />
       </Button>
