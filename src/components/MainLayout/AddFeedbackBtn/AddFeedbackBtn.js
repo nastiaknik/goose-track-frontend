f3b10a8f4252f0559f92f639ff17d4e536d439f8
andrewfriday13 <110158654+andrewfriday13@users.noreply.github.com>
2023-06-25 12:18:04 +0200

    coment addfeedback

--- src/components/MainLayout/AddFeedbackBtn/AddFeedbackBtn.js
+++ src/components/MainLayout/AddFeedbackBtn/AddFeedbackBtn.js
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 // import AddFeedbackModal from "./AddFeedbackModal";
-import { FeedbackButton } from "./AddFeedbackBtn.styled";
+// import { FeedbackButton } from "./AddFeedbackBtn.styled";
 
 const AddFeedbackBtn = () => {
-  const [isModalOpen, setModalOpen] = useState(false);
+  // const [isModalOpen, setModalOpen] = useState(false);
 
-  const handleButtonClick = () => {
-    setModalOpen(true);
-  };
+  // const handleButtonClick = () => {
+  //   setModalOpen(true);
+  // };
 
-  const handleCloseModal = () => {
-    setModalOpen(false);
-  };
+  // const handleCloseModal = () => {
+  //   setModalOpen(false);
+  // };
 
   return (
     <div>
-      <FeedbackButton onClick={handleButtonClick}>Feedback</FeedbackButton>
+      {/* <FeedbackButton onClick={handleButtonClick}>Feedback</FeedbackButton> */}
       {/* {isModalOpen && <AddFeedbackModal onCloseModal={handleCloseModal} />} */}
     </div>
   );
