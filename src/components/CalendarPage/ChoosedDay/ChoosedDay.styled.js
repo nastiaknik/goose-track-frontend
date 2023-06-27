6d8074a747afe60474476a85fb1b9799fe623111
AleksPastukhov <pastukhov.os@icloud.com>
2023-06-28 01:25:59 +0300

    added ChoosedDay, ChoosedMonth, calendar head

--- src/components/CalendarPage/ChoosedDay/ChoosedDay.styled.js
+++ src/components/CalendarPage/ChoosedDay/ChoosedDay.styled.js
@@ -1,10 +1,68 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 14px;
-  @media (min-width:768px){
+  @media (min-width: 768px) {
     gap: 16px;
+  }
+`;
+
+export const Day = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const SelectedDate = styled.span`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 1.17;
+  color: ${({ theme }) => theme.variableColors.calendarTextColor};
+  margin-top: 6px;
+  padding: 4px 6px;
+
+  @media screen and (min-width: 768px) {
+    padding: 4px 8px;
+    margin-top: 4px;
+    font-size: 16px;
+    line-height: 1.12;
+  }
+`;
+
+export const SelectedDay = styled.span`
+  background: ${({ theme }) => theme.variableColors.backgroundButton};
+  border-radius: 6px;
+  padding: 4px 6px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 1.17;
+  margin-top: 6px;
+  color: ${({ theme }) => theme.variableColors.textButton};
+
+  @media screen and (min-width: 768px) {
+    padding: 4px 8px;
+    margin-top: 4px;
+    font-size: 16px;
+    line-height: 1.12;
+  }
+`;
+
+export const DayOfWeek = styled.span`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 1.12;
+  text-transform: uppercase;
+  color: ${({ theme }) => theme.variableColors.calendarTextColor};
+
+  @media screen and (min-width: 768px) {
+    line-height: 1.29;
   }
 `;
