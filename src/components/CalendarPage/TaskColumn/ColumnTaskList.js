e1220a40dec8c6aa21433b9e232add18be065a7a
primarch93 <sugonegor@gmail.com>
2023-07-01 12:45:17 +0300

    Fumctions-tasks

--- src/components/CalendarPage/TaskColumn/ColumnTaskList.js
+++ src/components/CalendarPage/TaskColumn/ColumnTaskList.js
@@ -1,27 +1,38 @@
-/* import { useSelector } from 'react-redux';
- */import { TaskColumnCard } from 'components/CalendarPage/TaskColumnCard/TaskColumnCard';
+import { TaskColumnCard } from 'components/CalendarPage/TaskColumnCard/TaskColumnCard';
 import { TasksList } from './ColumnTaskList.styled';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectMonthTasks, selectDayTasks } from '../../../redux/tasks/selectors';
+import { getTasksByMonth, getTasksByDay } from 'redux/tasks/operations';
+import { useEffect } from 'react';
 
 export const ColumnsTasksList = ({ status }) => {
-/*   const tasks = useSelector(state => state.calendar.tasks); */
-/* 
-  const indexCurrentDay = useSelector(state => state.calendar.indexCurrentDay);
-  const dayTasks = tasks[indexCurrentDay]; */
+  const dispatch = useDispatch();
+  const monthTasks = useSelector(selectMonthTasks);
+  const dayTasks = useSelector(selectDayTasks);
 
-/*   let filteredDayTasks;
+  useEffect(() => {
+    const getData = async () => {
+      await dispatch(getTasksByMonth());
+      await dispatch(getTasksByDay()); 
+    };
+    getData();
+  }, [dispatch]);
+
+  let filteredDayTasks;
   if (dayTasks?.length > 0) {
     filteredDayTasks = dayTasks.filter(task => task.status === status.name);
-  } */
+  }
 
   return (
     <>
       <TasksList>
-       {/*  {filteredDayTasks &&
-          filteredDayTasks.map(task => ( */}
-           {/*  <li key={task._id}> */}
-              <TaskColumnCard /* {...task} */ />
-            {/* </li> */}
-         {/*  ))} */}
+        {monthTasks.map(task => (
+          <TaskColumnCard key={task._id} title={task.title} priority={task.priority} />
+        ))}
+        {filteredDayTasks &&
+          filteredDayTasks.map(task => (
+            <TaskColumnCard key={task._id} title={task.title} priority={task.priority} />
+          ))}
       </TasksList>
     </>
   );
