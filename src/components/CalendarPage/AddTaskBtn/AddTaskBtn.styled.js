5658928db7e51a1d98434683f6b135c38ba40bc8
IrynaVal <piv8050@gmail.com>
2023-07-04 22:15:44 +0300

    fix AddTaskBtn styles

--- src/components/CalendarPage/AddTaskBtn/AddTaskBtn.styled.js
+++ src/components/CalendarPage/AddTaskBtn/AddTaskBtn.styled.js
@@ -4,24 +4,23 @@ import { BiPlus } from "react-icons/bi";
 export const Button = styled.button`
   margin-left: auto;
   margin-right: auto;
-  padding: 16px 100px;
+  padding: 16px 0;
   border-radius: var(--border-radius);
   border: var(--border-add-task-btn);
   background-color: ${({ theme }) => theme.variableColors.buttonBlue};
-  text-align: center;
-  display: inline-block;
+  display: flex;
   color: ${({ theme }) => theme.variableColors.text};
   font-family: "InterSemiBold";
   font-size: 14px;
   font-weight: 600;
-  min-width: 299px;
-   transition: background-color var(--animation), box-shadow var(--animation);
+  width: 100%;
+  justify-content: center;
+  transition: background-color var(--animation), box-shadow var(--animation);
   &:hover,
   :focus {
     box-shadow: 4px 2px 16px rgba(136, 165, 191, 1);
-    background-color:${({theme})=>theme.variableColors.activeButtonBlue};
+    background-color: ${({ theme }) => theme.variableColors.activeButtonBlue};
   }
-
 
   @media screen and (min-width: 768px) {
     min-width: 304px;
@@ -35,7 +34,7 @@ export const PlusSvg = styled(BiPlus)`
   fill: ${({ theme }) => theme.variableColors.text};
 `;
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.span`
   display: flex;
   gap: 10px;
   align-items: center;
